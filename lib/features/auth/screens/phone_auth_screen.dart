a459fb61963f670873edd982c18dea5b0f851e2e
Apex by Skyner <apex@skyner.co.za>
2026-03-01 23:44:12 +0200

    SKY-51: Fix lint warnings

--- lib/features/auth/screens/phone_auth_screen.dart
+++ lib/features/auth/screens/phone_auth_screen.dart
@@ -169,7 +169,7 @@ class _PhoneAuthScreenState extends ConsumerState<PhoneAuthScreen> {
                     'https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg',
                     height: 20,
                     width: 20,
-                    errorBuilder: (_, __, ___) => const Icon(
+                    errorBuilder: (_, _, _)  => const Icon(
                       Icons.g_mobiledata,
                       size: 24,
                     ),
